fe77bfc15c00453fb7f66a2055ac9f9dda085c20
Yasaman Tavakkoli Tabasi <jasminetavakkoli@gmail.com>
2023-07-30 08:26:57 +0330

    Add files via upload

---
+++ session6 - JS/test.js
@@ -0,0 +1,37 @@
+
+// for (let index = 0; index < point.length; index++) {
+//     console.log(point[index])   
+// }
+
+
+
+// let i = 0;
+// while (i<4) {
+//     console.log(point[i])
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     console.log(point[i])
+//     i++;
+// } while (i<4);
+
+point = [1,10,20,40]
+for (let index = 0; index < point.length; index++) {
+    if(point[index] === 10){
+        continue;
+    }   
+    console.log(point[index])
+}
+
+
+
+// point.forEach(item => {
+//     console.log(item+1)
+// });
+
+// for (p in point) {
+//     console.log(p);
+// }
+
